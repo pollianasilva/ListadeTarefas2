7981d13f0cb1d9eeb756cebff86499b5c8063a44
pollianasilva <polly.cr@gmail.com>
2024-09-16 23:04:00 -0300

    feat: Agrupar quantidade total de produtos por cliente

--- 11.js
+++ 11.js
@@ -4,4 +4,24 @@
 cliente, produto, e quantidade. Use forEach para criar um objeto que
 agrupa a quantidade total de produtos por cliente.*/
 
+const pedidos = [
+    { cliente: 'Marcelo', produto: 'Feijão', quantidade: 5 },
+    { cliente: 'Joana', produto: 'Arroz', quantidade: 3 },
+    { cliente: 'Patricia', produto: 'Macarrão', quantidade: 2 },
+    { cliente: 'Joana', produto: 'Feijão', quantidade: 4 },
+    { cliente: 'Marcelo', produto: 'Arroz', quantidade: 7 },
+];
 
+const totalPorCliente = {};
+
+pedidos.forEach(pedido => {
+  
+    if (totalPorCliente[pedido.cliente]) {
+        totalPorCliente[pedido.cliente] += pedido.quantidade;
+    } else {        
+        totalPorCliente[pedido.cliente] = pedido.quantidade;
+    }
+});
+
+
+console.log(totalPorCliente);
