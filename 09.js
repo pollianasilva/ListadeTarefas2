cd32304328f4b612d016a56e22605724acc15870
pollianasilva <polly.cr@gmail.com>
2024-09-16 23:02:16 -0300

    feat: Contar clientes com mais de 30 anos

--- 09.js
+++ 09.js
@@ -3,3 +3,22 @@
 ○ Objetivo: Crie um array de objetos clientes, cada um com propriedades
 nome, idade, e cidade. Use forEach para contar quantos clientes têm mais
 de 30 anos. */
+
+const clientes = [
+    { nome: 'Patricia', idade: 25, cidade: 'Acre' },
+    { nome: 'Gustavo', idade: 18, cidade: 'Rio de Janeiro' },
+    { nome: 'Joana', idade: 38, cidade: 'Belo Horizonte' },
+    { nome: 'Pedro', idade: 40, cidade: 'Curitiba' },
+    { nome: 'Marcelo', idade: 50, cidade: 'Porto Alegre' },
+    { nome: 'Afonso', idade: 32, cidade: 'Palmas' }
+];
+
+let contador = 0;
+
+clientes.forEach(cliente => {
+    if (cliente.idade > 30) {
+        contador++; 
+    }
+});
+
+console.log(`Número de clientes com mais de 30 anos: ${contador}`);
