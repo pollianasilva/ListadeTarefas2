6186d0c7835f393e339d4ac3412ce75dbfcf759c
pollianasilva <polly.cr@gmail.com>
2024-09-16 23:05:59 -0300

    feat: Exibir funcionários por departamento

--- 14.js
+++ 14.js
@@ -6,3 +6,42 @@ de funcionarios. Use for in e for of para iterar sobre os departamentos
 e seus funcionários, exibindo o nome de cada funcionário junto com o
 departamento ao qual pertence. */
 
+const empresa = {
+    departamentos: [
+        {
+            nome: 'Avengers',
+            funcionarios: [
+                { nome: 'Tony Stark', cargo: 'Iron Man' },
+                { nome: 'Wanda Maximoff', cargo: 'Scarlet Witch' },
+                { nome: 'Peter Parker', cargo: 'Spider-Man' }
+            ]
+        },
+        {
+            nome: 'X-Men',
+            funcionarios: [
+                { nome: 'Anna Marie', cargo: 'Rogue' },
+                { nome: 'Logan', cargo: 'Wolverine' },
+                { nome: 'Jean Grey', cargo: 'Phoenix' }
+            ]
+        },
+        {
+            nome: 'Justice League',
+            funcionarios: [
+                { nome: 'Bruce Wayne', cargo: 'Batman' },
+                { nome: 'Diana Prince', cargo: 'Wonder Woman' },
+                { nome: 'Barry Allen', cargo: 'Flash' }
+            ]
+        }
+    ]
+};
+
+for (let i in empresa.departamentos) {
+    const departamento = empresa.departamentos[i];
+    console.log(`Departamento: ${departamento.nome}`);
+ 
+    for (let funcionario of departamento.funcionarios) {
+        console.log(`Funcionário: ${funcionario.nome}, Cargo: ${funcionario.cargo}`);
+    }
+
+    console.log(''); 
+}
