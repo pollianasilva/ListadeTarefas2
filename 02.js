c589302dd354d93f39d7d22228a1c8df727d19cc
pollianasilva <polly.cr@gmail.com>
2024-09-16 22:54:49 -0300

    feat: Verificar e adicionar propriedade ao objeto livro

--- 02.js
+++ 02.js
@@ -4,3 +4,33 @@
 anoPublicacao e genero. Verifique se a propriedade editora existe no
 objeto usando for in. Se não existir, adicione essa propriedade ao objeto.  */
 
+let livro = {
+    titulo: 'Pollyanna',
+    autor: 'Eleanor H. Porter',
+    anoPublicacao: '1913',
+}
+
+console.log('Antes de adicionar a editora:');
+for (let objeto in livro) {
+    console.log(objeto + ': ' + livro[objeto]);
+}
+
+//verificar se editora existe
+let propriedadeEditoraExiste = false;
+
+for (let objeto in livro) {
+    if (objeto === 'editora') {
+        propriedadeEditoraExiste = true;
+
+    }
+}
+
+// Se "editora" não existir, adiciona a propriedade
+if (!propriedadeEditoraExiste) {
+    livro['editora'] = 'Autentica';
+}
+
+console.log('\nApós adicionar a editora:');
+for (let objeto in livro) {
+    console.log(objeto + ':' + livro[objeto]);
+}
