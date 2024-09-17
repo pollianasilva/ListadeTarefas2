3ea4c4326ee228489b35901606f4ff7c3c2c9e85
pollianasilva <polly.cr@gmail.com>
2024-09-16 22:53:23 -0300

    feat: Adicionar acesso às propriedades do objeto carro

--- 01.js
+++ 01.js
@@ -2,7 +2,7 @@
 /*1. Acessando Propriedades de Objetos 
 ○ Objetivo: Crie um objeto carro com propriedades como marca, modelo,
 ano, e cor. Use for in para iterar sobre as propriedades e exibir seus
-valores no console.*/ 
+valores no console. */ 
 
 const prompt = require('prompt-sync')();
 
