509eb123ea3db1c6aafdb3cef2a7ecf582350bc0
pollianasilva <polly.cr@gmail.com>
2024-09-16 23:06:32 -0300

    feat: Calcular saldo final com base nas transações

--- 15.js
+++ 15.js
@@ -4,3 +4,21 @@
 tipo (entrada ou saída) e valor. Use forEach para calcular o saldo final,
 somando as entradas e subtraindo as saídas. */
 
+const transacoes = [
+    { tipo: 'entrada', valor: 1000 },
+    { tipo: 'saída', valor: 50.50 },
+    { tipo: 'entrada', valor: 200 },
+    { tipo: 'saída', valor: 30.00 }
+];
+
+let saldoFinal = 0;
+
+transacoes.forEach(transacao => {
+    if (transacao.tipo === 'entrada') {
+        saldoFinal += transacao.valor; 
+    } else if (transacao.tipo === 'saída') {
+        saldoFinal -= transacao.valor; 
+    }
+});
+
+console.log(`Saldo final: R$${saldoFinal.toFixed(2)}`);
