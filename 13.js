485eb40691dc2f445c1ea4180e66615639784256
pollianasilva <polly.cr@gmail.com>
2024-09-16 23:05:29 -0300

    feat: Calcular valor total do carrinho de compras

--- 13.js
+++ 13.js
@@ -5,3 +5,18 @@ array de objetos. Cada objeto dentro de itens deve representar um
 produto no carrinho, com nome, quantidade, e precoUnitario. Use forEach
 para calcular o valor total do carrinho. */
 
+const carrinho = {
+    itens: [
+        { nome: 'Feijão', quantidade: 2, precoUnitario: 10.00 },
+        { nome: 'Arroz', quantidade: 1, precoUnitario: 7.50 },
+        { nome: 'Macarrão', quantidade: 3, precoUnitario: 2.00 }
+    ]
+};
+
+let valorTotal = 0;
+
+carrinho.itens.forEach(item => {
+    valorTotal += item.quantidade * item.precoUnitario; 
+});
+
+console.log(`Valor total do carrinho: R$${valorTotal.toFixed(2)}`);
