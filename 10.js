47f0c8d6e3906f29b510c4db71e5fcdd9c89c021
pollianasilva <polly.cr@gmail.com>
2024-09-16 23:03:22 -0300

    feat: Calcular valor total das vendas

--- 10.js
+++ 10.js
@@ -3,3 +3,18 @@
 ○ Objetivo: Crie um array de objetos vendas, onde cada objeto tem produto,
 quantidade e valor. Use forEach para calcular o valor total de vendas de
 todos os produtos. */
+
+const vendas = [
+    { produto: 'Feijão', quantidade: 50, valor: 10.00 },
+    { produto: 'Arroz', quantidade: 30, valor: 7.50 },
+    { produto: 'Macarrão', quantidade: 25, valor: 2 },
+    { produto: 'Extrato de Tomate', quantidade: 40, valor: 3 }
+];
+
+let valorTotal = 0;
+
+vendas.forEach(venda => {
+    valorTotal += venda.quantidade * venda.valor; 
+});
+
+console.log(`Valor total das vendas: R$${valorTotal}`);
