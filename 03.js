c9f582e671e63f6d4d123325fd70dce2267861a8
pollianasilva <polly.cr@gmail.com>
2024-09-16 22:55:37 -0300

    feat: Filtrar propriedades de objeto produto

--- 03.js
+++ 03.js
@@ -4,3 +4,29 @@
 função que retorna um novo objeto contendo apenas as propriedades cujo
 valor seja maior que um valor específico. Use for in para filtrar as
 propriedades. */
+
+const produto = {
+    nome: "Smartv",
+    preco: 1200,
+    estoque: 50,
+    desconto: 0.10,
+    garantia: 12, 
+  };
+  
+  function filtrarPropriedades(obj, valorMinimo) {
+    const novoObjeto = {};
+  
+    for (let propriedade in obj) {
+      if (
+        typeof obj[propriedade] === "number" &&
+        obj[propriedade] > valorMinimo
+      ) {
+        novoObjeto[propriedade] = obj[propriedade];
+      }
+    }
+  
+    return novoObjeto;
+  }
+  
+  const resultado = filtrarPropriedades(produto, 50.1);
+  console.log(resultado);
