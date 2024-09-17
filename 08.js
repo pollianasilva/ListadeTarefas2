0c72ae0dfce1d2ef7cdfef4b884716fd6e471f3c
pollianasilva <polly.cr@gmail.com>
2024-09-16 23:01:11 -0300

    feat: Criar array com títulos dos filmes

--- 08.js
+++ 08.js
@@ -4,3 +4,17 @@
 diretor, e anoLancamento. Use forEach para criar um novo array
 contendo apenas os títulos dos filmes. */
 
+const filmes = [
+    { titulo: 'O Senhor dos Anéis', diretor: 'Peter Jackson', anoLancamento: 2001 },
+    { titulo: 'Matrix', diretor: 'Lana Wachowski, Lilly Wachowski', anoLancamento: 1999 },
+    { titulo: 'Inception', diretor: 'Christopher Nolan', anoLancamento: 2010 },
+    { titulo: 'Interstellar', diretor: 'Christopher Nolan', anoLancamento: 2014 }
+];
+
+let titulosDosFilmes = [];
+
+filmes.forEach(filme => {
+    titulosDosFilmes.push(filme.titulo); 
+});
+
+console.log(titulosDosFilmes);
