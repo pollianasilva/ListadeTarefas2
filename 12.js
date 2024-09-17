c9707256292c2ae64d57685223055f740e1f7de6
pollianasilva <polly.cr@gmail.com>
2024-09-16 23:04:41 -0300

    feat: Atualizar quantidade dos produtos no estoque

--- 12.js
+++ 12.js
@@ -4,3 +4,18 @@
 produto, quantidade e minimo. Use forEach para atualizar a quantidade
 dos produtos que estão abaixo do mínimo, duplicando suas quantidades. */
 
+const estoque = [
+    { produto: 'Feijão', quantidade: 5, minimo: 10 },
+    { produto: 'Arroz', quantidade: 12, minimo: 15 },
+    { produto: 'Macarrão', quantidade: 8, minimo: 10 },
+    { produto: 'Extrato de Tomate', quantidade: 20, minimo: 5 }
+];
+
+estoque.forEach(item => {
+    if (item.quantidade < item.minimo) {
+        item.quantidade *= 2; 
+    }
+});
+
+console.log(estoque);
+
